8286e2a7e0b7e029d9ad82c6de077d72f212e442
rliechti <rliechti@adobe.com>
2023-08-23 15:19:09 +0200

    add initial removal of header and footer

---
+++ tools/importer/transformers/index.js
@@ -0,0 +1,6 @@
+import removeHeaderAndFooter from './removeHeaderAndFooter.js';
+
+export const transformers = [removeHeaderAndFooter];
+export const asyncTransformers = [];
+export const preTransformers = [];
+export const postTransformers = [];
---
+++ tools/importer/transformers/removeHeaderAndFooter.js
@@ -0,0 +1,4 @@
+/* global WebImporter */
+export default function process(main) {
+  WebImporter.DOMUtils.remove(main, ['#header-outer','#header-space', '#footer-outer','#ageGateScreen','.nectar-skip-to-content','.slide-out-from-right']);
+}
