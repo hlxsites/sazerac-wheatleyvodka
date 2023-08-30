93f9ea681aa9b362a775b8f505c6845799cb5600
Karl Pauls <kpauls@adobe.com>
2023-08-30 11:54:33 +0200

    Set title automatically

--- tools/importer/transformers/index.js
+++ tools/importer/transformers/index.js
@@ -1,9 +1,8 @@
 import removeHeaderAndFooter from './removeHeaderAndFooter.js';
 import cocktails from './cocktails.js';
 import links from './links.js';
-import metadata from './metadata.js';
 
 export const transformers = [removeHeaderAndFooter, cocktails];
 export const asyncTransformers = [];
 export const preTransformers = [];
-export const postTransformers = [links, metadata];
+export const postTransformers = [links];
