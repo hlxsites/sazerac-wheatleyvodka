cf675ae6fbd00679943025f53dbc87f0c18c36ca
Karl Pauls <kpauls@adobe.com>
2023-09-05 10:22:52 +0200

    Finish hero styling (#57)

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -7,6 +7,7 @@ export default async function decorate(block) {
   const h1 = block.querySelector('h1');
   const h2 = block.querySelector('h2');
   const a = block.querySelector('a');
+  const h3 = block.querySelector('h3');
 
   if (picture && h1 && h2 && a) {
     block.innerHTML = '';
@@ -23,5 +24,23 @@ export default async function decorate(block) {
     block.appendChild(right);
 
     block.classList.add('no-background');
+
+    if (h3) {
+      const footer = document.createElement('div');
+      footer.classList.add('hero-footer');
+      footer.appendChild(h3);
+      block.parentElement.appendChild(footer);
+
+      let timeout;
+      const updateScroll = () => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          footer.classList.toggle('scrolled', window.innerWidth >= 1000 && block.offsetHeight - window.scrollY < 70);
+        }, 1);
+      };
+
+      window.addEventListener('scroll', updateScroll);
+      window.addEventListener('resize', updateScroll);
+    }
   }
 }
