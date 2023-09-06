5483268feeaac1cec64d77622cbbcafb338cc542
rliechti <rliechti@adobe.com>
2023-09-06 13:54:18 +0200

    initial version of carousel

--- blocks/quote-carousel/quote-carousel.js
+++ blocks/quote-carousel/quote-carousel.js
@@ -1,5 +1,20 @@
+let pressed = false;
+let startX;
+let x;
+
+const sliderContainer = document.querySelector('.quote-carousel-wrapper');
+const innerSlider = document.querySelector('.quote-carousel');
+
 function move(event) {
-  event.target.style.transform = 'translateX(42px)';
+  if (!pressed) return;
+  event.preventDefault();
+
+  x = event.offsetX;
+  if (event.type === 'touchmove') {
+    const r = sliderContainer.getBoundingClientRect();
+    x = (event.touches[0].clientX - r.left);
+  }
+  innerSlider.style.left = `${x - startX}px`;
 }
 
 export default async function decorate(block) {
@@ -14,7 +29,38 @@ export default async function decorate(block) {
 
     const iconSpan = document.createElement('span');
     iconSpan.className = 'quote-icon';
-    quote.querySelector('div').prepend(iconSpan);
-    quote.addEventListener('click', move);
+
+    const innderDiv = quote.querySelector('div');
+    const childs = innderDiv.childNodes;
+    quote.append(...childs);
+    innderDiv.remove();
+    quote.prepend(iconSpan);
+    quote.classList.add('quotecard');
   });
+
+  sliderContainer.addEventListener('mousedown', (e) => {
+    pressed = true;
+    startX = e.offsetX - innerSlider.offsetLeft;
+    sliderContainer.style.cursor = 'grabbing';
+  });
+
+  sliderContainer.addEventListener('mouseenter', () => {
+    sliderContainer.style.cursor = 'grab';
+  });
+
+  sliderContainer.addEventListener('mouseup', () => {
+    sliderContainer.style.cursor = 'grab';
+    pressed = false;
+  });
+  sliderContainer.addEventListener('mousemove', move);
+
+  sliderContainer.addEventListener('touchstart', (e) => {
+    pressed = true;
+    const r = sliderContainer.getBoundingClientRect();
+    startX = (e.touches[0].clientX - r.left) - innerSlider.offsetLeft;
+  });
+  sliderContainer.addEventListener('touchend', () => {
+    pressed = false;
+  });
+  sliderContainer.addEventListener('touchmove', move);
 }
