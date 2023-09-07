6af41be98fad70cf1e34a048854a505626bd7b21
Karl Pauls <kpauls@adobe.com>
2023-09-07 13:30:07 +0200

    Refactor to use template

--- blocks/buy/buy.js
+++ blocks/buy/buy.js
@@ -1,24 +1,38 @@
-export default function decorate(block) {
-  block.innerHTML = `
-  <picture class="mobile">
-      <source type="image/webp" srcset="./media_146775dcc1a0f644d54e8f00e8d223b0e49b3fd65.png?width=2000&amp;format=webply&amp;optimize=medium" media="(min-width: 600px)">
-      <source type="image/webp" srcset="./media_146775dcc1a0f644d54e8f00e8d223b0e49b3fd65.png?width=750&amp;format=webply&amp;optimize=medium">
-      <source type="image/png" srcset="./media_146775dcc1a0f644d54e8f00e8d223b0e49b3fd65.png?width=2000&amp;format=png&amp;optimize=medium" media="(min-width: 600px)">
-      <img loading="lazy" alt="" src="./media_146775dcc1a0f644d54e8f00e8d223b0e49b3fd65.png?width=750&amp;format=png&amp;optimize=medium" width="1129" height="1213">
-    </picture>
-  <picture class="desktop">
-      <source type="image/webp" srcset="./media_19259e012ae71de2fc4382102cdc78354f373c7e6.png?width=2000&amp;format=webply&amp;optimize=medium" media="(min-width: 600px)">
-      <source type="image/webp" srcset="./media_19259e012ae71de2fc4382102cdc78354f373c7e6.png?width=750&amp;format=webply&amp;optimize=medium">
-      <source type="image/png" srcset="./media_19259e012ae71de2fc4382102cdc78354f373c7e6.png?width=2000&amp;format=png&amp;optimize=medium" media="(min-width: 600px)">
-      <img loading="lazy" alt="" src="./media_19259e012ae71de2fc4382102cdc78354f373c7e6.png?width=750&amp;format=png&amp;optimize=medium" width="4321" height="1950">
-    </picture>
-  <div class="buy-wood-panel">
-    <div class="title">Get Wheatley delivered to your doorstep</div>
-    <div class="buttons">
-      <a class="button" href="https://drizly.com/liquor/vodka/wheatley-vodka/p35302" target="_blank">Buy Online<span/></a>
-      <br>
-      <a class="locator" href="/locator">Product Locator<span/></a>
-    </div>
-  </div>
-  `;
+import { decorateButtons, decorateIcons, toClassName } from '../../scripts/lib-franklin.js';
+
+export default async function decorate(block) {
+  block.textContent = '';
+
+  const resp = await fetch('/templates/buy.plain.html', window.location.pathname.endsWith('/templates/buy') ? { cache: 'reload' } : {});
+
+  if (!resp.ok) return;
+
+  const html = await resp.text();
+  block.innerHTML = html;
+  block.querySelectorAll('.buy-template > div').forEach((row) => {
+    if (row.children) {
+      const cols = [...row.children];
+      let blockLevel = false;
+      if (cols[1]) {
+        const col = cols[1];
+        const name = toClassName(cols[0].textContent);
+        col.classList.add(name);
+        blockLevel = name.startsWith('block-');
+      }
+      row.replaceWith(cols[1]);
+      if (blockLevel) {
+        cols[1].parentElement.parentElement.append(cols[1]);
+      }
+    } else {
+      row.remove();
+    }
+  });
+  decorateButtons(block);
+  const buttons = document.createElement('div');
+  buttons.classList.add('buttons');
+  buttons.append(...block.querySelectorAll('.button-container'));
+  block.querySelector('.title').insertAdjacentElement('afterend', buttons);
+  block.querySelector('.buy-link').append(block.querySelector('.buy-link-image'));
+  block.querySelector('.locator-link').append(block.querySelector('.locator-link-image'));
+  await decorateIcons(block);
 }
