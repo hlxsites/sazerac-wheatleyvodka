206f333c98139a7cca686ff85c4857cc04e91613
rliechti <rliechti@adobe.com>
2023-09-08 11:21:52 +0200

    steps

--- blocks/steps/steps.js
+++ blocks/steps/steps.js
@@ -2,7 +2,7 @@ export default async function decorate(block) {
   [...block.children].forEach((row, index) => {
     const step = document.createElement('div');
     step.classList.add('step');
-    step.innerHTML = index + 1;
+    step.innerHTML = `<span>${index + 1}</span>`;
     row.firstElementChild.prepend(step);
   });
 }
