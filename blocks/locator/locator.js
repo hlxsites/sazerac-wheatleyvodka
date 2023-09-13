edc0f477f6dcb8cb8f9ea08d3f7d048d2952c02a
Carsten Ziegeler <cziegeler@apache.org>
2023-09-13 11:58:02 +0200

    Move locator div out of main and apply button style only to main buttons

--- blocks/locator/locator.js
+++ blocks/locator/locator.js
@@ -5,10 +5,21 @@ export default async function decorate() {
   locator.className = 'wheatley-mikmak';
   locator.style = 'visibility: hidden';
   locator.dataset.mikmakUpcs = '088004019761,088004022686,088004027834,088004022679,088004034184,088004034191,088004040574';
-  // append locator before section
+  // split main into two parts
+  const main2 = document.createElement('main');
   const section = document.querySelector('.section.locator-container');
-  section.before(locator);
-  locator.before(loader);
+  // move all siblings of section into main2
+  while (section.nextSibling) {
+    const s = section.nextSibling;
+    s.style = '';
+    main2.appendChild(s);
+  }
+  // append loader before section
+  section.before(loader);
   // remove section
   section.remove();
+  // append locator after main
+  const main = document.querySelector('main');
+  main.after(locator);
+  locator.after(main2);
 }
